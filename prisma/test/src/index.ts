cc0cb68a4ff1e505ddfec79f2a8a9b9145ae9543
Ridoy Chandra Dey <iamridoydey@gmail.com>
2025-01-03 21:42:09 +0600

    prisma basic

--- prisma/test/src/index.ts
+++ prisma/test/src/index.ts
@@ -1,1 +1,66 @@
-console.log("Hello Prisma")
+import { PrismaClient } from "@prisma/client";
+const client = new PrismaClient();
+
+async function createUser() {
+  try {
+    await client.user.create({
+      data: {
+        username: "Prite",
+        password: "iamprite",
+        age: 25,
+        city: "Dhaka",
+      },
+    });
+    console.log("User created successfully.");
+  } catch (error) {
+    console.error("Error creating user:", error);
+  }
+}
+
+async function deleteUser() {
+  try {
+    await client.user.delete({
+      where: { username: "Prite" },
+      
+    });
+    console.log("User deleted successfully.");
+  } catch (error) {
+    console.error("Error deleting user:", error);
+  }
+}
+
+async function updateUser() {
+  try {
+    await client.user.update({
+      where: {
+        username: "Prite",
+      },
+      data: {
+        username: "iampritedey",
+      },
+    });
+    console.log("User updated successfully.");
+  } catch (error) {
+    console.error("Error updating user:", error);
+  }
+}
+
+async function findUser() {
+  try {
+    const users = await client.user.findMany({
+      select: {
+        username: true,
+        todos: true
+      }
+    });
+    console.log(JSON.stringify(users));
+  } catch (error) {
+    console.error("Error finding users:", error);
+  }
+}
+
+// Uncomment the desired function calls to test them out
+// createUser();
+// deleteUser();
+// updateUser();
+findUser();
